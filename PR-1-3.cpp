29d328b3a704280457ac405deaa8a4ea10dd67dc
sg1687054-design <sg1687054@gmail.com>
2026-04-06 15:14:00 +0530

    Add files via upload

---
+++ PR-1-3.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+	int  angle1, angle2, angle3;
+	
+	printf("enter value of angle1");
+	scanf("%d", &angle1);
+	
+	printf("enter value of angle2");
+	scanf("%d", &angle2);
+	
+	angle3 = 180 - (angle1 + angle2);
+	
+	printf("angle3 = %d\n", angle3);
+}
